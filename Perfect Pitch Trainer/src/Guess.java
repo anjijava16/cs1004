849901ada0cbf9dc540ee0c69ae2d1f529245e82
Kai-Zhan Lee <kaizhan.lee@gmail.com>
2014-07-12 21:35:18 -0400

    Modified Perfect Pitch Trainer

--- Perfect Pitch Trainer/src/Guess.java
+++ Perfect Pitch Trainer/src/Guess.java
@@ -3,176 +3,209 @@ import java.util.Scanner;
 public class Guess {
 
 	public static boolean isUserRight(String userGuess, int pitchNumber) {
-		if((userGuess.equals("A") || userGuess.equals("a") || userGuess.equals("A natural") || userGuess.equals("a natural")) && pitchNumber == 0) {
+		if ((userGuess.equals("A") || userGuess.equals("a") ||
+				userGuess.equals("A natural") || userGuess.equals("a natural")) &&
+				pitchNumber == 0)
 			return true;
-		}
-		else if((userGuess.equals("A#") || userGuess.equals("Bb") || userGuess.equals("A sharp") || userGuess.equals("B flat") || userGuess.equals("a sharp") || userGuess.equals("b flat")) && pitchNumber == 1) {
+		else if ((userGuess.equals("A#") || userGuess.equals("Bb") ||
+				userGuess.equals("A sharp") || userGuess.equals("B flat") ||
+				userGuess.equals("a sharp") || userGuess.equals("b flat")) &&
+				pitchNumber == 1)
 			return true;
-		}
-		else if((userGuess.equals("B") || userGuess.equals("b") || userGuess.equals("B natural") || userGuess.equals("b natural")) && pitchNumber == 2) {
+		else if ((userGuess.equals("B") || userGuess.equals("b") ||
+				userGuess.equals("B natural") || userGuess.equals("b natural")) &&
+				pitchNumber == 2)
 			return true;
-		}
-		else if((userGuess.equals("C") || userGuess.equals("c") || userGuess.equals("C natural") || userGuess.equals("c natural")) && pitchNumber == 3) {
+		else if ((userGuess.equals("C") || userGuess.equals("c") ||
+				userGuess.equals("C natural") || userGuess.equals("c natural")) &&
+				pitchNumber == 3)
 			return true;
-		}
-		else if((userGuess.equals("C#") || userGuess.equals("Db") || userGuess.equals("C sharp") || userGuess.equals("D flat") || userGuess.equals("c sharp") || userGuess.equals("d flat")) && pitchNumber == 4) {
+		else if ((userGuess.equals("C#") || userGuess.equals("Db") ||
+				userGuess.equals("C sharp") || userGuess.equals("D flat") ||
+				userGuess.equals("c sharp") || userGuess.equals("d flat")) &&
+				pitchNumber == 4)
 			return true;
-		}
-		else if((userGuess.equals("D") || userGuess.equals("d") || userGuess.equals("D natural") || userGuess.equals("d natural")) && pitchNumber == 5) {
+		else if ((userGuess.equals("D") || userGuess.equals("d") ||
+				userGuess.equals("D natural") || userGuess.equals("d natural")) &&
+				pitchNumber == 5)
 			return true;
-		}
-		else if((userGuess.equals("D#") || userGuess.equals("Eb") || userGuess.equals("D sharp") || userGuess.equals("E flat") || userGuess.equals("d sharp") || userGuess.equals("e flat")) && pitchNumber == 6) {
+		else if ((userGuess.equals("D#") || userGuess.equals("Eb") ||
+				userGuess.equals("D sharp") || userGuess.equals("E flat") ||
+				userGuess.equals("d sharp") || userGuess.equals("e flat")) &&
+				pitchNumber == 6)
 			return true;
-		}
-		else if((userGuess.equals("E") || userGuess.equals("e") || userGuess.equals("E natural") || userGuess.equals("e natural")) && pitchNumber == 7) {
+		else if ((userGuess.equals("E") || userGuess.equals("e") ||
+				userGuess.equals("E natural") || userGuess.equals("e natural")) &&
+				pitchNumber == 7)
 			return true;
-		}
-		else if((userGuess.equals("F") || userGuess.equals("f") || userGuess.equals("F natural") || userGuess.equals("f natural")) && pitchNumber == 8) {
+		else if ((userGuess.equals("F") || userGuess.equals("f") ||
+				userGuess.equals("F natural") || userGuess.equals("f natural")) &&
+				pitchNumber == 8)
 			return true;
-		}
-		else if((userGuess.equals("F#") || userGuess.equals("Gb") || userGuess.equals("F sharp") || userGuess.equals("G flat") || userGuess.equals("f sharp") || userGuess.equals("g flat")) && pitchNumber == 9) {
+		else if ((userGuess.equals("F#") || userGuess.equals("Gb") ||
+				userGuess.equals("F sharp") || userGuess.equals("G flat") ||
+				userGuess.equals("f sharp") || userGuess.equals("g flat")) &&
+				pitchNumber == 9)
 			return true;
-		}
-		else if((userGuess.equals("G") || userGuess.equals("g") || userGuess.equals("G natural") || userGuess.equals("g natural")) && pitchNumber == 10) {
+		else if ((userGuess.equals("G") || userGuess.equals("g") ||
+				userGuess.equals("G natural") || userGuess.equals("g natural")) &&
+				pitchNumber == 10)
 			return true;
-		}
-		else if((userGuess.equals("G#") || userGuess.equals("Ab") || userGuess.equals("G sharp") || userGuess.equals("A flat") || userGuess.equals("g sharp") || userGuess.equals("a flat")) && pitchNumber == 11) {
+		else if ((userGuess.equals("G#") || userGuess.equals("Ab") ||
+				userGuess.equals("G sharp") || userGuess.equals("A flat") ||
+				userGuess.equals("g sharp") || userGuess.equals("a flat")) &&
+				pitchNumber == 11)
 			return true;
-		}
-		else {
+		else
 			return false;
-		}
 	}
 
 	public static boolean isUserWrong(String userGuess, int pitchNumber) {
-		if(!userGuess.equals("Play pitch again") && !userGuess.equals("I give up") && !userGuess.equals("Exit")) {
-			if((!userGuess.equals("A") && !userGuess.equals("a") && !userGuess.equals("A natural") && !userGuess.equals("a natural")) && pitchNumber == 0) {
+		if (!userGuess.equals("Play pitch again") &&
+				!userGuess.equals("I give up") && !userGuess.equals("Exit")) {
+			if (!userGuess.equals("A") && !userGuess.equals("a") &&
+					!userGuess.equals("A natural") &&
+					!userGuess.equals("a natural") && pitchNumber == 0)
 				return true;
-			}
-			else if((!userGuess.equals("A#") && !userGuess.equals("Bb") && !userGuess.equals("A sharp") && !userGuess.equals("B flat") && !userGuess.equals("a sharp") && !userGuess.equals("b flat")) && pitchNumber == 1) {
+			else if (!userGuess.equals("A#") && !userGuess.equals("Bb") &&
+					!userGuess.equals("A sharp") &&
+					!userGuess.equals("B flat") &&
+					!userGuess.equals("a sharp") &&
+					!userGuess.equals("b flat") && pitchNumber == 1)
 				return true;
-			}
-			else if((!userGuess.equals("B") && !userGuess.equals("b") && !userGuess.equals("B natural") && !userGuess.equals("b natural")) && pitchNumber == 2) {
+			else if (!userGuess.equals("B") && !userGuess.equals("b") &&
+					!userGuess.equals("B natural") &&
+					!userGuess.equals("b natural") && pitchNumber == 2)
 				return true;
-			}
-			else if((!userGuess.equals("C") && !userGuess.equals("c") && !userGuess.equals("C natural") && !userGuess.equals("c natural")) && pitchNumber == 3) {
+			else if (!userGuess.equals("C") && !userGuess.equals("c") &&
+					!userGuess.equals("C natural") &&
+					!userGuess.equals("c natural") && pitchNumber == 3)
 				return true;
-			}
-			else if((!userGuess.equals("C#") && !userGuess.equals("Db") && !userGuess.equals("C sharp") && !userGuess.equals("D flat") && !userGuess.equals("c sharp") && !userGuess.equals("d flat")) && pitchNumber == 4) {
+			else if (!userGuess.equals("C#") && !userGuess.equals("Db") &&
+					!userGuess.equals("C sharp") &&
+					!userGuess.equals("D flat") &&
+					!userGuess.equals("c sharp") &&
+					!userGuess.equals("d flat") && pitchNumber == 4)
 				return true;
-			}
-			else if((!userGuess.equals("D") && !userGuess.equals("d") && !userGuess.equals("D natural") && !userGuess.equals("d natural")) && pitchNumber == 5) {
+			else if (!userGuess.equals("D") && !userGuess.equals("d") &&
+					!userGuess.equals("D natural") &&
+					!userGuess.equals("d natural") && pitchNumber == 5)
 				return true;
-			}
-			else if((!userGuess.equals("D#") && !userGuess.equals("Eb") && !userGuess.equals("D sharp") && !userGuess.equals("E flat") && !userGuess.equals("d sharp") && !userGuess.equals("e flat")) && pitchNumber == 6) {
+			else if (!userGuess.equals("D#") && !userGuess.equals("Eb") &&
+					!userGuess.equals("D sharp") &&
+					!userGuess.equals("E flat") &&
+					!userGuess.equals("d sharp") &&
+					!userGuess.equals("e flat") && pitchNumber == 6)
 				return true;
-			}
-			else if((!userGuess.equals("E") && !userGuess.equals("e") && !userGuess.equals("E natural") && !userGuess.equals("e natural")) && pitchNumber == 7) {
+			else if (!userGuess.equals("E") && !userGuess.equals("e") &&
+					!userGuess.equals("E natural") &&
+					!userGuess.equals("e natural") && pitchNumber == 7)
 				return true;
-			}
-			else if((!userGuess.equals("F") && !userGuess.equals("f") && !userGuess.equals("F natural") && !userGuess.equals("f natural")) && pitchNumber == 8) {
+			else if (!userGuess.equals("F") && !userGuess.equals("f") &&
+					!userGuess.equals("F natural") &&
+					!userGuess.equals("f natural") && pitchNumber == 8)
 				return true;
-			}
-			else if((!userGuess.equals("F#") && !userGuess.equals("Gb") && !userGuess.equals("F sharp") && !userGuess.equals("G flat") && !userGuess.equals("f sharp") && !userGuess.equals("g flat")) && pitchNumber == 9) {
+			else if (!userGuess.equals("F#") && !userGuess.equals("Gb") &&
+					!userGuess.equals("F sharp") &&
+					!userGuess.equals("G flat") &&
+					!userGuess.equals("f sharp") &&
+					!userGuess.equals("g flat") && pitchNumber == 9)
 				return true;
-			}
-			else if((!userGuess.equals("G") && !userGuess.equals("g") && !userGuess.equals("G natural") && !userGuess.equals("g natural")) && pitchNumber == 10) {
+			else if (!userGuess.equals("G") && !userGuess.equals("g") &&
+					!userGuess.equals("G natural") &&
+					!userGuess.equals("g natural") && pitchNumber == 10)
 				return true;
-			}
-			else if((!userGuess.equals("G#") && !userGuess.equals("Ab") && !userGuess.equals("G sharp") && !userGuess.equals("A flat") && !userGuess.equals("g sharp") && !userGuess.equals("a flat")) && pitchNumber == 11) {
+			else if (!userGuess.equals("G#") && !userGuess.equals("Ab") &&
+					!userGuess.equals("G sharp") &&
+					!userGuess.equals("A flat") &&
+					!userGuess.equals("g sharp") &&
+					!userGuess.equals("a flat") && pitchNumber == 11)
 				return true;
-			}
-			else {
+			else
 				return false;
-			}
-		}
-		else {
+		} else
 			return false;
-		}
 	}
-	
+
 	public static void askContinue() {
-		while(true) {
-			System.out.println("Would you like to continue? Please say either 'Yes' or 'No'");
+		while (true) {
+			System.out
+					.println("Would you like to continue? Please say either 'Yes' or 'No'");
 			Scanner scanner = new Scanner(System.in);
 			String userContinue = scanner.nextLine();
-			if(userContinue.equals("Yes") | userContinue.equals("yes")) {
+			if (userContinue.equals("Yes") | userContinue.equals("yes"))
 				break;
-			}
-			else if(userContinue.equals("No") | userContinue.equals("no")) {
+			else if (userContinue.equals("No") | userContinue.equals("no")) {
 				System.out.println("Okay. Goodbye!");
 				System.exit(1);
-			}
-			else {
+			} else
 				System.out.println("You didn't follow instructions.");
-			}
 		}
 	}
 
 	public static void askTryAgain() {
-		System.out.println("Would you like to try again? Please say either 'Yes' or 'No'");
+		System.out
+				.println("Would you like to try again? Please say either 'Yes' or 'No'");
 		Scanner scanner = new Scanner(System.in);
 		String userTryAgain = scanner.nextLine();
-		if(userTryAgain.equals("Yes") | userTryAgain.equals("yes")) {
+		if (userTryAgain.equals("Yes") | userTryAgain.equals("yes")) {
 			System.out.println("");
 			return;
-		}
-		else if(userTryAgain.equals("No") | userTryAgain.equals("no")) {
+		} else if (userTryAgain.equals("No") | userTryAgain.equals("no")) {
 			System.out.println("Okay. Goodbye!");
 			System.exit(1);
-		}
-		else {
+		} else {
 			System.out.println("You didn't follow instructions.");
 			askTryAgain();
 		}
 	}
 
 	public static void tellAnswer(int pitchNumber) {
-		if(pitchNumber == 0) {
+		if (pitchNumber == 0) {
 			System.out.println("The pitch was an A.");
 			askTryAgain();
 		}
-		if(pitchNumber == 1) {
+		if (pitchNumber == 1) {
 			System.out.println("The pitch was a Bb/A#.");
 			askTryAgain();
 		}
-		if(pitchNumber == 2) {
+		if (pitchNumber == 2) {
 			System.out.println("The pitch was a B.");
 			askTryAgain();
 		}
-		if(pitchNumber == 3) {
+		if (pitchNumber == 3) {
 			System.out.println("The pitch was a C.");
 			askTryAgain();
 		}
-		if(pitchNumber == 4) {
+		if (pitchNumber == 4) {
 			System.out.println("The pitch was a C#.");
 			askTryAgain();
 		}
-		if(pitchNumber == 5) {
+		if (pitchNumber == 5) {
 			System.out.println("The pitch was a D.");
 			askTryAgain();
 		}
-		if(pitchNumber == 6) {
+		if (pitchNumber == 6) {
 			System.out.println("The pitch was an Eb/D#.");
 			askTryAgain();
 		}
-		if(pitchNumber == 7) {
+		if (pitchNumber == 7) {
 			System.out.println("The pitch was an E.");
 			askTryAgain();
 		}
-		if(pitchNumber == 8) {
+		if (pitchNumber == 8) {
 			System.out.println("The pitch was an F.");
 			askTryAgain();
 		}
-		if(pitchNumber == 9) {
+		if (pitchNumber == 9) {
 			System.out.println("The pitch was an F#/Gb.");
 			askTryAgain();
 		}
-		if(pitchNumber == 10) {
+		if (pitchNumber == 10) {
 			System.out.println("The pitch was a G.");
 			askTryAgain();
 		}
-		if(pitchNumber == 11) {
+		if (pitchNumber == 11) {
 			System.out.println("The pitch was an Ab/G#.");
 			askTryAgain();
 		}
--- Perfect Pitch Trainer/src/Main.java
+++ Perfect Pitch Trainer/src/Main.java
@@ -1,44 +1,46 @@
-import java.util.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 	static Random randomGen = new Random();
 
 	public static void main(String[] args) {
-		System.out.println("What pitch is this? Type what pitch you think it is.");
-		while(true) {
+		System.out
+				.println("What pitch is this? Type what pitch you think it is.");
+		while (true) {
 			int pitchNumber = randomGen.nextInt(12);
-			//The commented code below will tell you what note is played, so you can test what happens when the user answers correctly.
-			//System.out.println(PlaySound.files[pitchNumber]);
-			while(true) {
+			// The commented code below will tell you what note is played, so
+			// you can test what happens when the user answers correctly.
+			// System.out.println(PlaySound.files[pitchNumber]);
+			while (true) {
 				PlaySound.playPitch(pitchNumber);
-				System.out.println("Type 'Play pitch again' to hear the pitch another time.");
-				System.out.println("Type 'I give up' if you want the answer, or 'Exit' if you want to stop.");
+				System.out
+						.println("Type 'Play pitch again' to hear the pitch another time.");
+				System.out
+						.println("Type 'I give up' if you want the answer, or 'Exit' if you want to stop.");
 				Scanner scanner = new Scanner(System.in);
 				String userGuess = scanner.nextLine();
-				if(Guess.isUserRight(userGuess, pitchNumber)) {
+				scanner.close();
+				if (Guess.isUserRight(userGuess, pitchNumber)) {
 					System.out.println("Correct! Nice job!");
 					Guess.askContinue();
 					PlaySound.playSequence();
 					break;
-				}
-				else if(Guess.isUserWrong(userGuess, pitchNumber)) {
+				} else if (Guess.isUserWrong(userGuess, pitchNumber)) {
 					System.out.println("Sorry, that's incorrect!");
 					continue;
-				}
-				else if(userGuess.equals("I give up")) {
+				} else if (userGuess.equals("I give up")) {
 					Guess.tellAnswer(pitchNumber);
 					PlaySound.playSequence();
 					break;
-				}
-				else if(userGuess.equals("Play pitch again")) {
-				}
-				else if(userGuess.equals("Exit")) {
+				} else if (userGuess.equals("Play pitch again")) {} else if (userGuess
+						.equals("Exit")) {
 					System.out.println("Okay. Goodbye!");
 					System.exit(1);
-				}
-				else {
+				} else {
 					System.out.println("You didn't follow the instructions.");
-					System.out.println("What pitch is this? Type what pitch you think it is.");
+					System.out
+							.println("What pitch is this? Type what pitch you think it is.");
 				}
 			}
 		}
